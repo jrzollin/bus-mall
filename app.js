7adc0cb0981eed23e93947bf89d8bbb22f31a510
jrzollin <jrzollin@yahoo.com>
2017-06-24 15:37:23 -0700

    no repitition for current or previous displays

--- app.js
+++ app.js
@@ -11,24 +11,68 @@ function Product(name, image){
 }
 
 function display(){
-  //generates 3 numbers and checks if they are different
   var repeat = true;
-  while(repeat === true){
+  var previousRepeat1 = true;
+  var previousRepeat2 = true;
+  var previousRepeat3 = true;
+
+  while(repeat === true || previousRepeat1 === true || previousRepeat2 === true || previousRepeat3 === true){
+    //generates 3 numbers
     var number1 = Math.round(Math.random() * 19);
     var number2 = Math.round(Math.random() * 19);
     var number3 = Math.round(Math.random() * 19);
+
     console.log('first number: ' + number1);
     console.log('second number: ' + number2);
-    console.log('thrid number: ' + number3);
+    console.log('third number: ' + number3);
 
+
+    //checks if the 3 numbers repeat
     if(number1 !== number2 && number1 !== number3 && number2 !== number3){
-      var repeat = false;
+      repeat = false;
     }
+
+    //checks if first number matches any previous numbers
+    if(number1 !== previousDisplay[0] && number1 !== previousDisplay[1] && number1 !== previousDisplay[2]){
+      previousRepeat1 = false;
+    }
+
+    //checks if second number matches any previous numbers
+    if(number2 !== previousDisplay[0] && number2 !== previousDisplay[1] && number2 !== previousDisplay[2]){
+      previousRepeat2 = false;
+    }
+
+    //checks if third number matches any previous numbers
+    if(number3 !== previousDisplay[0] && number3 !== previousDisplay[1] && number3 !== previousDisplay[2]){
+      previousRepeat3 = false;
+    }
+
+    //checks if all values were set to false
+    if(repeat === true || previousRepeat1 === true || previousRepeat2 === true || previousRepeat3 === true){
+      repeat = true;
+      previousRepeat1 = true;
+      previousRepeat2 = true;
+      previousRepeat3 = true;
+    }
+  }
+
+  console.log('previous numbers: ' + previousDisplay);
+  previousDisplay.splice(0, 1, number1);
+  previousDisplay.splice(1, 1, number2);
+  previousDisplay.splice(2, 1, number3);
+
+
+  //check for and remove previous display
+  if(document.getElementById('exhibit1')){
+    var child = document.getElementById('exhibit1');
+    var parent = child.parentNode;
+    parent.removeChild(child);
   }
 
   //create figure element 1
   var exhibit1 = document.createElement('figure');
   exhibit1.setAttribute('class', 'exhibit');
+  exhibit1.setAttribute('id', 'exhibit1');
   displayArea.appendChild(exhibit1);
 
   //create image element 1
@@ -44,9 +88,17 @@ function display(){
   exhibit1.appendChild(image1TextEl);
 
 
+  //check for and remove previous display
+  if(document.getElementById('exhibit2')){
+    var child = document.getElementById('exhibit2');
+    var parent = child.parentNode;
+    parent.removeChild(child);
+  }
+
   //create figure element 2
   var exhibit2 = document.createElement('figure');
   exhibit2.setAttribute('class', 'exhibit');
+  exhibit2.setAttribute('id', 'exhibit2');
   displayArea.appendChild(exhibit2);
 
   //create image element 2
@@ -61,9 +113,17 @@ function display(){
   image2TextEl.textContent = products[number2]['name'];
   exhibit2.appendChild(image2TextEl);
 
+  //check for and remove previous display
+  if(document.getElementById('exhibit3')){
+    var child = document.getElementById('exhibit3');
+    var parent = child.parentNode;
+    parent.removeChild(child);
+  }
+
   //create figure element 3
   var exhibit3 = document.createElement('figure');
   exhibit3.setAttribute('class', 'exhibit');
+  exhibit3.setAttribute('id', 'exhibit3');
   displayArea.appendChild(exhibit3);
 
   //create image element 3
@@ -77,6 +137,15 @@ function display(){
   var image3TextEl = document.createElement('figcaption');
   image3TextEl.textContent = products[number3]['name'];
   exhibit3.appendChild(image3TextEl);
+
+  var productImage1 = document.getElementById('image1');
+  productImage1.addEventListener('click', handleClick);
+
+  var productImage2 = document.getElementById('image2');
+  productImage2.addEventListener('click', handleClick);
+
+  var productImage3 = document.getElementById('image3');
+  productImage3.addEventListener('click', handleClick);
 }
 
 function handleClick(event){
@@ -97,7 +166,7 @@ var bathroom = new Product('Bathroom Stand', 'img/bathroom.jpg');
 var boots = new Product('Toeless Boots', 'img/boots.jpg');
 var breakfast = new Product('Breakfast Maker', 'img/breakfast.jpg');
 var bubblegum = new Product('Meatball Bubblegum', 'img/bubblegum.jpg');
-var chair = new Product('"Comfortable Chair"', 'img/chair.jpg');
+var chair = new Product('"Comfortable" Chair', 'img/chair.jpg');
 var cthulhu = new Product('Cthulhu Action Figure', 'img/cthulhu.jpg');
 var dogDuck = new Product('Dog-Duck Mask', 'img/dog-duck.jpg');
 var dragon = new Product('Dragon Meat', 'img/dragon.jpg');
@@ -116,12 +185,3 @@ var wineGlass = new Product('Designer Wine Glass', 'img/wine-glass.jpg');
 var displayArea = document.getElementById('display-area');
 
 display();
-
-var productImage1 = document.getElementById('image1');
-productImage1.addEventListener('click', handleClick);
-
-var productImage2 = document.getElementById('image2');
-productImage2.addEventListener('click', handleClick);
-
-var productImage3 = document.getElementById('image3');
-productImage3.addEventListener('click', handleClick);
